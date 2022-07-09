85f9d9282bd4d014af125eda4bb509e9449b20db
Sailendra <sailendra.maharjan@rumsan.com>
2022-07-09 19:02:37 +0545

    responsive in mobile

--- global/header.php
+++ global/header.php
@@ -1,9 +1,9 @@
 <nav class="navbar navbar-dark bg-dark p-3 header">
   <div class="row">
-    <div class="col-6">
+    <div class="col-sm-6 col-md-6">
       <a class="navbar-brand" href="/">IIMS Blog</a>
     </div>
-    <div class="col-6 text-end">
+    <div class="col-sm-6 col-md-6 text-end">
       <a class="btn navbar-item" href="contact-us.php">Contact Us</a>
       <a class="btn btn-success btn-sm" href="login.php">Login</a>
     </div>
--- index.php
+++ index.php
@@ -12,7 +12,7 @@
 
   <h6 class="m-3">Recent Blogs</h6>
   <div class="row">
-    <div class="col-3">
+    <div class="col-sm-12 col-md-4 col-lg-3">
       <div class="card">
         <a href="/blog-detail.php" class="a-link">
           <img class="card-img-top" src="images/default.png" alt="Preview">
