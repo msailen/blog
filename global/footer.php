a037c2823fbfde3d4963a6b22a0decdd6d39ba73
Sailendra <sailendra.maharjan@rumsan.com>
2022-07-09 19:08:24 +0545

    fix footer overlap

--- global/footer.php
+++ global/footer.php
@@ -1,5 +1,5 @@
 <!-- Container Div -->
 </div>
-<div class="footer">
+<div class="footer mt-5">
   This website is a college project developed by <u><b>Bidhan C. Bhattarai</b></u>, <u><b>Jah Bir Shakya</b></u> & <u><b>Sailendra Maharjan</b></u>
 </div>
