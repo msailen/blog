c54743898765570953f5af2f9249965b6e972294
Sailendra <sailendra.maharjan@rumsan.com>
2022-07-12 16:04:17 +0545

    contact us add done

--- contact-us.php
+++ contact-us.php
@@ -1,4 +1,31 @@
-<?php session_start() ?>
+<?php
+session_start();
+$error = false;
+$success = false;
+$formValue['name'] = '';
+$formValue['email'] = '';
+$formValue['message'] = '';
+
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+
+  $formValue['name'] = $_POST['name'];
+  $formValue['email'] =  $_POST['email'];
+  $formValue['message'] =  $_POST['message'];
+
+  require_once $_SERVER['DOCUMENT_ROOT'] . "/controllers/feedback.controller.php";
+
+  if (!empty($_POST['name']) && !empty($_POST['email']) && !empty($_POST['message']) && !$error) {
+    $result = Feedback::addFeedback($formValue);
+    if (!$result) {
+      $error = "Failed To Send Your Message";
+      return;
+    }
+    $success = "We have received your message. Thanks for contacting Us.";
+  } else {
+    $error = "All Fields Are Required";
+  }
+}
+?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -17,14 +44,20 @@
       <div class="card contact-us-card">
         <div class="card-body">
           <h5 class="card-title text-center">Contact Us</h5>
-          <form>
+          <form method="POST" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
+            <span class="text-danger"><?php if (isset($error)) echo $error; ?></span>
+            <span class="text-success"><?php if (isset($success)) echo $success; ?></span>
             <div class="form-group mb-2">
-              <label for="subject">Subject</label>
-              <input type="text" class="form-control" id="subject" placeholder="Subject" required>
+              <label for="name">Name</label>
+              <input type="text" class="form-control" name="name" placeholder="Your Name" required>
+            </div>
+            <div class="form-group mb-2">
+              <label for="email">Email</label>
+              <input type="email" class="form-control" name="email" placeholder="Your Email" required>
             </div>
             <div class="form-group mb-2">
               <label for="message">Message</label>
-              <textarea type="text" rows="5" class="form-control" id="message" placeholder="Your Message" required></textarea>
+              <textarea type="text" rows="5" class="form-control" name="message" placeholder="Your Message" required></textarea>
             </div>
             <button type="submit" class="btn btn-success mt-2">Submit</button>
           </form>
---
+++ controllers/feedback.controller.php
@@ -0,0 +1,37 @@
+<?php
+
+require_once $_SERVER['DOCUMENT_ROOT'] . "/utils/connection.php";
+
+class Feedback
+{
+
+  public static function addFeedback($payload)
+  {
+    try {
+      $conn = DB::getConnection();
+      $sql = "INSERT INTO feedbacks (name, email, message) VALUES (?, ?, ?)";
+      $stmt = $conn->prepare($sql);
+      $stmt->bind_param("sss", $payload['name'], $payload['email'], $payload['message']);
+      $stmt->execute();
+      $stmt->close();
+      return true;
+    } catch (\Throwable $th) {
+      return false;
+    }
+  }
+
+  public static function listFeedbacks()
+  {
+    $conn = DB::getConnection();
+    $sql = "SELECT * FROM feedbacks ORDER BY id DESC";
+    $stmt = $conn->prepare($sql);
+    $stmt->execute();
+    $result = $stmt->get_result();
+    while ($row = $result->fetch_assoc()) {
+      $feedbacks[] = $row;
+    }
+    $stmt->close();
+    $conn->close();
+    return $feedbacks;
+  }
+}
