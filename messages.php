fbb3d393194b6dddd8791e245999754b3ce50fe5
Sailendra <sailendra.maharjan@rumsan.com>
2022-07-12 18:04:45 +0545

    bug fixes

--- messages.php
+++ messages.php
@@ -1,15 +1,10 @@
 <?php
 session_start();
-$error = false;
 if (!$_SESSION['isAdmin']) {
   header("location: 404.php");
 }
 require_once $_SERVER['DOCUMENT_ROOT'] . "/controllers/feedback.controller.php";
 $feedbacks = Feedback::listFeedbacks();
-if (!$feedbacks) {
-  $error = "Failed To Fetch Messages";
-  return;
-}
 ?>
 
 <!DOCTYPE html>
@@ -26,22 +21,24 @@ if (!$feedbacks) {
 
   <div class="row">
     <?php
-    foreach ($feedbacks as $feedback) {
+    if ($feedbacks) {
+      foreach ($feedbacks as $feedback) {
     ?>
-      <div class="col-12 mb-2">
-        <div class="card">
-          <div class="card-body">
-            <h5 class="card-title"> <?php echo $feedback['name'] . " [ " . $feedback['email'] . " ]" ?></h5>
-            <span>
-              <?php echo $feedback['created_at'] ?>
-            </span>
-            <div class="comment-text">
-              <?php echo $feedback['message'] ?>
+        <div class="col-12 mb-2">
+          <div class="card">
+            <div class="card-body">
+              <h5 class="card-title"> <?php echo $feedback['name'] . " [ " . $feedback['email'] . " ]" ?></h5>
+              <span>
+                <?php echo $feedback['created_at'] ?>
+              </span>
+              <div class="comment-text">
+                <?php echo $feedback['message'] ?>
+              </div>
             </div>
           </div>
         </div>
-      </div>
     <?php
+      }
     }
     ?>
   </div>
