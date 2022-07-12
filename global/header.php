a779e76bc9deb9eb0b9af790a83d94152977be9b
Sailendra <sailendra.maharjan@rumsan.com>
2022-07-12 16:17:08 +0545

    added messages UI

--- global/header.php
+++ global/header.php
@@ -4,6 +4,13 @@
       <a class="navbar-brand" href="/">IIMS Blog</a>
     </div>
     <div class="col-sm-12 col-md-6 text-end">
+      <?php
+      if (isset($_SESSION['isAdmin'])) {
+      ?>
+        <a class="btn navbar-item" href="/messages.php">Messages</a>
+      <?php
+      }
+      ?>
       <a class="btn navbar-item" href="/contact-us.php">Contact Us</a>
       <?php
       if (!isset($_SESSION['userId'])) {
---
+++ messages.php
@@ -0,0 +1,50 @@
+<?php
+session_start();
+$error = false;
+require_once $_SERVER['DOCUMENT_ROOT'] . "/controllers/feedback.controller.php";
+$feedbacks = Feedback::listFeedbacks();
+if (!$feedbacks) {
+  $error = "Failed To Fetch Messages";
+  return;
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <title>Contact Us</title>
+  <?php require_once "global/head.php"; ?>
+
+</head>
+
+<body>
+  <?php require_once "global/header.php"; ?>
+
+  <div class="row">
+    <?php
+    foreach ($feedbacks as $feedback) {
+    ?>
+      <div class="col-12 mb-2">
+        <div class="card">
+          <div class="card-body">
+            <h5 class="card-title"> <?php echo $feedback['name'] . " [ " . $feedback['email'] . " ]" ?></h5>
+            <span>
+              <?php echo $feedback['created_at'] ?>
+            </span>
+            <div class="comment-text">
+              <?php echo $feedback['message'] ?>
+            </div>
+          </div>
+        </div>
+      </div>
+    <?php
+    }
+    ?>
+  </div>
+
+  <?php require_once "global/footer.php"; ?>
+
+</body>
+
+</html>
